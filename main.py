99aed9320c8ab111a0073ebcb905c1d9eed03772
Madina Tashkenbay <madinatawkenbai@gmail.com>
2025-07-19 16:01:05 +0500

    Add new test functions

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import requests
 
 
-url = "https://reqres.in/api/users?page=2"
 def test_response_code():
+    url = "https://reqres.in/api/users?page=2"
     response = requests.get(url)
     assert response.status_code == 200
 
 def test_response_content():
+    url = "https://reqres.in/api/users?page=2"
     response = requests.get(url)
     print(response.content)
+
+def test_response_status_code_404():
+    url = "https://reqres.in/api/users/23"
+
+    response = requests.get(url)
+    assert response.status_code == 404
