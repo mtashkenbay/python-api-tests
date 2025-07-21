a3e39202085bbce1b2afb8ac1ffe4e0c64c04763
Madina Tashkenbay <madinatawkenbai@gmail.com>
2025-07-21 17:52:14 +0500

    Test Jenkins

--- python-jenkins-demo/tests/main.py
+++ python-jenkins-demo/tests/main.py
@@ -1,19 +1,20 @@
 import requests
+import json
 
 
 def test_response_code():
     url = "https://reqres.in/api/users?page=2"
     response = requests.get(url)
+    json_body = json.load(response.content)
+    print(json_body)
     assert response.status_code == 200
 
 def test_response_content():
     url = "https://reqres.in/api/users/2"
     response =requests.get(url).json()
     print(response)
-    print("1111!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11111")
 
 def test_response_status_code_404():
     url = "https://reqres.in/api/users/23"
-    print("hi")
     response = requests.get(url)
     assert response.status_code == 404
